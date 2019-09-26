96c6440a5ce57d7efc0cc1e63997ec7bf2b2a67a
tgrosh <tgrosh@yahoo.com>
2019-09-25 22:07:16 -0400

    Player Model and Running Animation

---
+++ Assets/Scripts/RunningAgent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RunningAgent : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Animator animator;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("running", agent.velocity.magnitude > .01f);
+    }
+}
